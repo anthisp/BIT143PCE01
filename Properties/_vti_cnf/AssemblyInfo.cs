46ce38e4ad12d9d8296c0cd3ba2243f241fe1eef
Pete Anthis <anthisp@gmail.com>
2017-09-28 12:04:19 -0700

    Setting up Repo
    Initial Setup

---
+++ Properties/_vti_cnf/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|05 Jan 2008 18:57:05 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_cacheddtm:TX|05 Jan 2008 18:57:05 -0000
+vti_filesize:IR|1312
+vti_backlinkinfo:VX|
